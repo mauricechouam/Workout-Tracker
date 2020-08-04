6f044a65ba6fa1381c9fea4450c2b848da2b30f6
mauricechouam <lansichouamou@gmail.com>
2020-08-03 21:28:13 -0700

    deleted exercise.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,20 @@
 var path = require("path");
 
-module.exports = function (app){
-// respond with index.html when a GET request is made to the homepage
-app.get("/", function(req,res){
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+module.exports = function (app) {
+    // respond with index.html when a GET request is made to the homepage
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
 
-//This route path will match requests to the root route,/exercise.
-app.get("/exercise", function (req,res){
-    res.sendFile(path.join(__dirname, "../public/exercise.html"));
-});
+    //This route path will match requests to the root route,/exercise.
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
 
 
-//This route path will match requests to the root route, /stats.
-app.get("/stats", function(req, res){
-    res.sendFile(path.join(__dirname, "../public/stats.html"));
-});
+    //This route path will match requests to the root route, /stats.
+    app.get("/stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
 }
